b6ba1a605c4d87ae5cf5e04aa29d8ab859850de5
Tadiwa Chipolopolo <tadiwanashenicckky@gmail.com>
2024-04-14 17:24:55 +0200

    added links

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -38,26 +38,33 @@ const Card = () => {
       </Typography>
 
       <Stack direction={"column"} gap={1} mt={2.5} mb={3}>
-        {["Github", "Frontend Mentor", "LinkedIn", "Twitter", "Instagram"].map(
-          (item, index) => (
-            <Button
-              sx={{
-                textTransform: "initial",
-                color: "hsl(0, 0%, 100%)",
-                bgcolor: "hsl(0, 0%, 20%)",
-                width: 250,
+        {[
+          { name: "Github", link: "https://github.com/Tadteck" },
+          { name: "Frontend Mentor", link: "https://www.frontendmentor.io/profile/Tadteck" },
+          { name: "LinkedIn", link: "https://www.linkedin.com/in/tadiwanashe-chipolopolo-9a4190304/" },
+          { name: "Twitter", link: "" },
+          { name: "Instagram", link: "" },
+        ].map((item, index) => (
+          <Button
+            onClick={() => {
+              window.open(item.link);
+            }}
+            sx={{
+              textTransform: "initial",
+              color: "hsl(0, 0%, 100%)",
+              bgcolor: "hsl(0, 0%, 20%)",
+              width: 250,
 
-                "&:hover" :{
-                    bgcolor:"hsl(75, 94%, 57%)",
-                    color:"hsl(0, 0%, 8%)"
-                }
-              }}
-              key={index}
-            >
-              {item}
-            </Button>
-          )
-        )}
+              "&:hover": {
+                bgcolor: "hsl(75, 94%, 57%)",
+                color: "hsl(0, 0%, 8%)",
+              },
+            }}
+            key={index}
+          >
+            {item.name}
+          </Button>
+        ))}
       </Stack>
     </Box>
   );
